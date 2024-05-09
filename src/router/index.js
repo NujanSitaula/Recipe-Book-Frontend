ee583452d7cc9c305dff04d51ffb9db611b9d59a
Sovia <59617775+soviaa@users.noreply.github.com>
2024-05-09 14:04:33 +0545

    login tried

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
+import Login from "@/views/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/sovia',
+      name: 'sovia',
+      component: Login
     }
   ]
 })
