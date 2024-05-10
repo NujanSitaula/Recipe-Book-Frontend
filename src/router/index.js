88e82b6e6902538c919d8bc9a0151e9245072c4a
NujanSitaula <nujan.sitaula@outlook.com>
2024-05-10 13:52:12 +0545

    Started working on frontend. Currently Working on LoginView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { IStaticMethods } from "preline/preline";
 
 import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 
 const router = createRouter({
@@ -17,6 +18,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
     }
   ]
 })
