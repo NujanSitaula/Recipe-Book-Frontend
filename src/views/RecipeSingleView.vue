5bb2684cf568d03bbbd0bc5a8a613e68c02de8e5
NujanSitaula <nujan.sitaula@outlook.com>
2024-05-18 14:27:17 +0545

    Search in popup implemented.
    Co-authored-by: Soviaa <soviamdr@gmail.com>

--- src/views/RecipeSingleView.vue
+++ src/views/RecipeSingleView.vue
@@ -325,7 +325,7 @@
                       class="mr-2 w-6 h-6 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
                       alt="Michael Gough">Michael Gough</p>
-                  <p class="text-sm text-gray-600"><time pubdate datetime="2022-02-08"
+                  <p class="text-sm text-gray-600"><time datetime="2022-02-08"
                                                          title="February 8th, 2022">Feb. 8, 2022</time></p>
                 </div>
                 <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
@@ -376,7 +376,7 @@
                       class="mr-2 w-6 h-6 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                       alt="Jese Leos">Jese Leos</p>
-                  <p class="text-sm text-gray-600"><time pubdate datetime="2022-02-12"
+                  <p class="text-sm text-gray-600"><time datetime="2022-02-12"
                                                          title="February 12th, 2022">Feb. 12, 2022</time></p>
                 </div>
                 <button id="dropdownComment2Button" data-dropdown-toggle="dropdownComment2"
@@ -426,7 +426,7 @@
                       class="mr-2 w-6 h-6 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
                       alt="Bonnie Green">Bonnie Green</p>
-                  <p class="text-sm text-gray-600"><time pubdate datetime="2022-03-12"
+                  <p class="text-sm text-gray-600"><time datetime="2022-03-12"
                                                          title="March 12th, 2022">Mar. 12, 2022</time></p>
                 </div>
                 <button id="dropdownComment3Button" data-dropdown-toggle="dropdownComment3"
