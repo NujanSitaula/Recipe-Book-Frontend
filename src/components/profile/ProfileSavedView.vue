63cddd15b59ef3103cb92a931b538c38772b9053
Sovia <59617775+soviaa@users.noreply.github.com>
2024-05-21 11:46:23 +0545

    profile update icon
    Co-authored-by: NujanSitaula <nujan.sitaula@outlook.com>

--- src/components/profile/ProfileSavedView.vue
+++ src/components/profile/ProfileSavedView.vue
@@ -3,36 +3,7 @@
 </template>
 
 <script>
-export default {
-  props: {
-    isOpen: {
-      type: Boolean,
-      required: true
-    }
-  },
-  data() {
-    return {
-      firstName: '',
-      lastName: '',
-      email: '',
-      tempImageUrl: ''
-    };
-  },
-  methods: {
-    closeModal() {
-      this.$emit('close');
-    },
-    handleFileUpload(event) {
-      const file = event.target.files[0];
-      if (file) {
-        this.tempImageUrl = URL.createObjectURL(file);
-      }
-    },
-    submitForm() {
-      // Handle form submission logic
-    }
-  }
-};
+
 </script>
 
 <style scoped>
