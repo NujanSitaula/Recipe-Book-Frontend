38705839f65472546cbe30d885fb767a9cc78287
Sovia <59617775+soviaa@users.noreply.github.com>
2024-05-29 13:39:13 +0545

    add recipe basics
    Co-authored-by: NujanSitual <nujan.siatula@outlook.com>

--- src/components/recipe/BasicRecipeView.vue
+++ src/components/recipe/BasicRecipeView.vue
@@ -1,56 +1,38 @@
 <template>
-  <div class="overflow-hidden">
-    <div class="mt-3 max-w-[60rem] mx-auto px-4 sm:px-6 lg:px-8 main-content">
-      <div class="main">
-        <!-- Card Section -->
-        <div class="max-w-4xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <!-- Card -->
           <div class="bg-white rounded-xl shadow p-4 sm:p-7">
             <form>
-              <!-- Section -->
-              <div class="grid sm:grid-cols-12 gap-2 sm:gap-4 py-8 first:pt-0 last:pb-0 border-t first:border-transparent border-gray-200">
                 <div class="sm:col-span-12">
-                  <h2 class="text-lg font-semibold text-center text-gray-800">
-                    Add your recipe here
-                  </h2>
+                  <h1 class="text-xl font-semibold text-gray-800">
+                    Lets start with the basics
+                  </h1>
                 </div>
+              <!-- Section -->
+              <div class="grid sm:grid-cols-12 gap-2 sm:gap-4 py-8 px-8 first:pt-0 last:pb-0 border-t first:border-transparent border-gray-200">
                 <!-- End Col -->
 
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-full-name" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
-                    Name of your recipe
+                <div class="sm:col-span-12">
+                  <label class=" m-1 text-sm font-medium text-gray-500 mt-2.5">
+                    Recipe Title
+                    <input type="text" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500" placeholder="Enter your recipe title here">
                   </label>
                 </div>
-                <!-- End Col -->
 
-                <div class="sm:col-span-9">
-                  <div class="sm:flex">
-                    <input type="text" class="block w-full p-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500">
-                  </div>
-                </div>
-                <!-- End Col -->
 
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-email" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
-                    Give a short description to your recipe
+                <div class="sm:col-span-12">
+                  <label class="m-1 text-sm font-medium text-gray-500 mt-2.5">
+                    Description
+                  <textarea class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500" placeholder="Give a short description to your recipe"></textarea>
                   </label>
                 </div>
-                <!-- End Col -->
-
-                <div class="sm:col-span-9">
-                  <textarea class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"></textarea>
-                </div>
-                <!-- End Col -->
 
 
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-email" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
+
+                <div class="sm:col-span-12">
+                  <label class="m-1 text-sm font-medium text-gray-500 mt-2.5">
                     Add image of your recipe
                   </label>
-                </div>
-
-                <div class="sm:col-span-9">
-                  <div class="flex items-center justify-center w-full">
+                  <div class="flex items-center justify-start w-64">
                     <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
                       <div class="flex flex-col items-center justify-center pt-5 pb-6">
                         <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
@@ -62,15 +44,13 @@
                       <input id="dropzone-file" type="file" class="hidden" />
                     </label>
                   </div>
-
                 </div>
 
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-email" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
+
+                <div class="sm:col-span-12">
+                  <label class="inline-block m-1 text-sm font-medium text-gray-500 mt-2.5">
                     Preparation Time
                   </label>
-                </div>
-                <div class="sm:col-span-9">
                   <div class="sm:grid-cols-2 inline-flex">
                     <div class="sm:col-span-1 inline-flex w-full">
                       <input type="text" class="mr-2 block w-full p-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500">
@@ -84,14 +64,12 @@
                         Minutes
                       </label>
                     </div>
-                  </div>
                 </div>
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-email" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
+                </div>
+                <div class="col-span-12">
+                  <label class="block m-1 text-sm font-medium text-gray-500 mt-2.5">
                     Cooking Time
                   </label>
-                </div>
-                <div class="sm:col-span-9">
                   <div class="sm:grid-cols-2 inline-flex">
                     <div class="sm:col-span-1 inline-flex w-full">
                       <input type="text" class="mr-2 block w-full p-1 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500">
@@ -105,69 +83,51 @@
                         Minutes
                       </label>
                     </div>
-                  </div>
                 </div>
-                <div class="sm:col-span-3">
-                  <label for="af-submit-application-email" class="inline-block text-sm font-medium text-gray-500 mt-2.5">
-                    Difficulty
+                </div>
+
+                <div class="sm:col-span-12">
+                  <label class=" m-1 text-sm font-medium text-gray-500 mt-2.5">
+                    Servings
+                    <input type="text" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500" placeholder="#">
                   </label>
                 </div>
-                <div class="sm:col-span-9">
 
-                  <div class="p-4">
-
-                    <ul class="grid w-full gap-6 md:grid-cols-3">
+                <div class="sm:col-span-12">
+                  <label class="m-1 text-sm font-medium text-gray-500 mt-2.5">
+                    Difficulty
+                  </label>
+                  <ul class="grid w-full h-full gap-6 md:grid-cols-3">
                       <li>
                         <input type="radio" id="easy" name="type_of_recipe" value="easy" class="hidden peer" required @change="" v-model="easy" />
-                        <label for="rookie" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center text-center justify-between w-full text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer ">
-                          <div class="block">
-                            <div class="w-full align- pt-2">Easy</div>
+                        <label for="easy" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center justify-between w-full  text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer ">
+                          <div class="w-full text-center p-2">
+                            Easy
                           </div>
                         </label>
                       </li>
                       <li>
                         <input type="radio" id="medium" name="type_of_recipe" value="midbie" class="hidden peer" required @change="" v-model="medium" />
-                        <label for="midbie" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer ">
-                          <div class="block">
-                            <div class="w-full text-lg font-semibold"></div>
-
-                            <div class="w-full text-center pt-2">Medium</div>
-                          </div>
+                        <label for="medium" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center justify-between w-full  text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer ">
+                            <div class="w-full text-center p-2">Medium</div>
                         </label>
                       </li>
                       <li>
                         <input type="radio" id="hard" name="type_of_recipe" value="master" class="hidden peer" required @change="" v-model="hard" />
-                        <label for="master" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer">
-                          <div class="block">
-                            <div class="w-full text-lg font-semibold"></div>
-
-                            <div class="w-full text-center pt-2">Hard</div>
-                          </div>
-
+                        <label for="hard" class="peer-checked:border-primary-100 peer-checked:border-2 peer-checked:text-primary-100 inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer">
+                          <div class="w-full text-center p-2">Hard</div>
                         </label>
                       </li>
                     </ul>
-                  </div>
+
                 </div>
 
 
               </div>
-              <!-- End Section -->
-
-
-
-              <button type="button" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary-100 text-white hover:bg-primary-200 disabled:opacity-50 disabled:pointer-events-none">
-                Save Recipe
-              </button>
             </form>
           </div>
           <!-- End Card -->
-        </div>
-        <!-- End Card Section -->
-      </div>
 
-    </div>
-  </div>
 </template>
 
 <script>
--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -1,80 +1,96 @@
 <template>
 
   <div class="overflow-hidden">
-    <div class="mt-3 max-w-[60rem] mx-auto px-4 sm:px-6 lg:px-8 main-content">
+    <div class="mt-5 max-w-[50rem] mx-auto px-4 sm:px-6 lg:px-8 main-content">
       <div class="main">
-        <div class="p-4 bg-white rounded-lg shadow-md">
+        <div class="p-4 bg-white rounded-lg">
           <!-- Stepper -->
           <div data-hs-stepper="">
             <!-- Stepper Nav -->
-            <ul class="relative flex flex-row gap-x-2">
-              <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group" data-hs-stepper-nav-item='{
+            <ul class=" flex flex-row gap-x-2 relative">
+              <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group"  data-hs-stepper-nav-item='{
         "index": 1
       }'>
         <span class="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
-          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-blue-600 hs-stepper-active:text-white hs-stepper-success:bg-blue-600 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
+          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-primary-100 hs-stepper-active:text-white hs-stepper-success:bg-primary-100 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
             <span class="hs-stepper-success:hidden hs-stepper-completed:hidden">1</span>
             <svg class="hidden flex-shrink-0 size-3 hs-stepper-success:block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
           </span>
           <span class="ms-2 text-sm font-medium text-gray-800">
-            Step
+            Basics
           </span>
         </span>
-                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-blue-600 hs-stepper-completed:bg-teal-600"></div>
+                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-primary-100 hs-stepper-completed:bg-teal-600"></div>
               </li>
 
               <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group" data-hs-stepper-nav-item='{
         "index": 2
       }'>
         <span class="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
-          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-blue-600 hs-stepper-active:text-white hs-stepper-success:bg-blue-600 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
+          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-primary-100 hs-stepper-active:text-white hs-stepper-success:bg-primary-100 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
             <span class="hs-stepper-success:hidden hs-stepper-completed:hidden">2</span>
             <svg class="hidden flex-shrink-0 size-3 hs-stepper-success:block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
           </span>
           <span class="ms-2 text-sm font-medium text-gray-800">
-            Step
+            Ingredients
           </span>
         </span>
-                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-blue-600 hs-stepper-completed:bg-teal-600"></div>
+                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-primary-100 hs-stepper-completed:bg-teal-600"></div>
               </li>
-
               <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group" data-hs-stepper-nav-item='{
-          "index": 3
-        }'>
+        "index": 3
+      }'>
         <span class="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
-          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-blue-600 hs-stepper-active:text-white hs-stepper-success:bg-blue-600 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
+          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-primary-100 hs-stepper-active:text-white hs-stepper-success:bg-primary-100 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
             <span class="hs-stepper-success:hidden hs-stepper-completed:hidden">3</span>
             <svg class="hidden flex-shrink-0 size-3 hs-stepper-success:block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
           </span>
           <span class="ms-2 text-sm font-medium text-gray-800">
-            Step
+            Recipe Steps
           </span>
         </span>
-                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-blue-600 hs-stepper-completed:bg-teal-600"></div>
+                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-primary-100 hs-stepper-completed:bg-teal-600"></div>
               </li>
+
+              <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group" data-hs-stepper-nav-item='{
+          "index": 4
+        }'>
+        <span class="min-w-7 min-h-7 group inline-flex items-center text-xs align-middle">
+          <span class="size-7 flex justify-center items-center flex-shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full group-focus:bg-gray-200 hs-stepper-active:bg-primary-100 hs-stepper-active:text-white hs-stepper-success:bg-primary-100 hs-stepper-success:text-white hs-stepper-completed:bg-teal-500 hs-stepper-completed:group-focus:bg-teal-600">
+            <span class="hs-stepper-success:hidden hs-stepper-completed:hidden">4</span>
+            <svg class="hidden flex-shrink-0 size-3 hs-stepper-success:block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+              <polyline points="20 6 9 17 4 12"></polyline>
+            </svg>
+          </span>
+          <span class="ms-2 text-sm font-medium text-gray-800">
+            Final Details
+          </span>
+        </span>
+                <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden hs-stepper-success:bg-primary-100 hs-stepper-completed:bg-teal-600"></div>
+              </li>
+
               <!-- End Item -->
             </ul>
             <!-- End Stepper Nav -->
 
             <!-- Stepper Content -->
             <div class="mt-5 sm:mt-8">
-              <!-- First Contnet -->
+<!--              add recipe basics-->
               <div data-hs-stepper-content-item='{
         "index": 1
       }'>
-                <div class="p-4 h-48 bg-gray-50 flex justify-center items-center border border-dashed border-gray-200 rounded-xl">
-                  <h3 class="text-gray-500">
-                    First content
-                  </h3>
+                <div class="h-auto bg-gray-50 flex justify-center items-center border border-dashed border-gray-200 rounded-xl">
+                  <router-view to="addRecipe/basics">
+                  </router-view>
                 </div>
               </div>
-              <!-- End First Contnet -->
+              <!-- end of add recipes basics -->
 
               <!-- First Contnet -->
               <div data-hs-stepper-content-item='{
@@ -86,15 +102,25 @@
                   </h3>
                 </div>
               </div>
-              <!-- End First Contnet -->
-
-              <!-- First Contnet -->
+              <!-- End First Content -->
               <div data-hs-stepper-content-item='{
         "index": 3
       }' style="display: none;">
                 <div class="p-4 h-48 bg-gray-50 flex justify-center items-center border border-dashed border-gray-200 rounded-xl">
                   <h3 class="text-gray-500">
                     Third content
+                  </h3>
+                </div>
+              </div>
+              <!-- End First Content -->
+
+              <!-- First Content -->
+              <div data-hs-stepper-content-item='{
+        "index": 4
+      }' style="display: none;">
+                <div class="p-4 h-48 bg-gray-50 flex justify-center items-center border border-dashed border-gray-200 rounded-xl">
+                  <h3 class="text-gray-500">
+                    Fourth content
                   </h3>
                 </div>
               </div>
@@ -120,16 +146,16 @@
                   </svg>
                   Back
                 </button>
-                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-next-btn="">
+                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-primary-100 text-white hover:bg-primary-200 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-next-btn="">
                   Next
                   <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="m9 18 6-6-6-6"></path>
                   </svg>
                 </button>
-                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-finish-btn="" style="display: none;">
+                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-primary-100 text-white hover:bg-primary-200 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-finish-btn="" style="display: none;">
                   Finish
                 </button>
-                <button type="reset" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-reset-btn="" style="display: none;">
+                <button type="reset" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-primary-100 text-white hover:bg-primary-200 disabled:opacity-50 disabled:pointer-events-none" data-hs-stepper-reset-btn="" style="display: none;">
                   Reset
                 </button>
               </div>
